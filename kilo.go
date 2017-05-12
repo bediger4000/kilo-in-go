25ce644141b409021c4228a258a8407ceb681283
Bruce Ediger <bediger@stratigery.com>
2017-05-11 21:41:07 -0600

    Step 5

--- kilo.go
+++ kilo.go
@@ -2,9 +2,43 @@ package main
 
 import (
 	"os"
+	"syscall"
+	"unsafe"
 )
 
+type Termios struct {
+	Iflag  uint32
+	Oflag  uint32
+	Cflag  uint32
+	Lflag  uint32
+	Cc     [20]byte
+	Ispeed uint32
+	Ospeed uint32
+}
+
+func TcSetAttr(fd uintptr, termios *Termios) error {
+	if _, _, err := syscall.Syscall(syscall.SYS_IOCTL, fd, uintptr(syscall.TCSETS), uintptr(unsafe.Pointer(termios))); err != 0 {
+		return err
+	}
+	return nil
+}
+
+func TcGetAttr(fd uintptr) (*Termios, error) {
+	var termios = &Termios{}
+	if _, _, err := syscall.Syscall(syscall.SYS_IOCTL, fd, syscall.TCGETS, uintptr(unsafe.Pointer(termios))); err != 0 {
+		return nil, err
+	}
+	return termios, nil
+}
+
+func enableRawMode() {
+	raw, _ := TcGetAttr(os.Stdin.Fd())
+	raw.Lflag &^= syscall.ECHO
+	_ = TcSetAttr(os.Stdin.Fd(), raw)
+}
+
 func main() {
+	enableRawMode()
 	buffer := make([]byte, 1)
 	for cc, err := os.Stdin.Read(buffer); buffer[0] != 'q' && err == nil && cc == 1; cc, err = os.Stdin.Read(buffer) {
 		// blank
