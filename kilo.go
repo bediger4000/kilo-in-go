380c7e380a503b7c52979f2041c399c219a81053
Bruce Ediger <bediger@stratigery.com>
2017-05-11 21:23:10 -0600

    Step 3

--- kilo.go
+++ kilo.go
@@ -1,7 +1,13 @@
 package main
+
 import (
 	"os"
 )
+
 func main() {
-	os.Exit(0);
+	buffer := make([]byte, 1)
+	for cc, err := os.Stdin.Read(buffer); err == nil && cc == 1; cc, err = os.Stdin.Read(buffer) {
+		// blank
+	}
+	os.Exit(0)
 }
