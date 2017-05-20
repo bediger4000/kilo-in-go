cb3902052110a355c22b0023f54a4fd3a1f8d03b
Bruce Ediger <bediger@stratigery.com>
2017-05-20 16:27:32 -0600

    Step 65

--- kilo.go
+++ kilo.go
@@ -239,15 +239,20 @@ func editorOpen(filename string) {
 	defer fd.Close()
 	fp := bufio.NewReader(fd)
 
-	line, err := fp.ReadBytes('\n')
-	if err != nil {
+	for line, err := fp.ReadBytes('\n'); err == nil; line, err = fp.ReadBytes('\n') { 
+		// Trim trailing newlines and carriage returns
+		for c := line[len(line) - 1]; len(line) > 0 && (c == '\n' || c == '\r'); {
+			line = line[:len(line)-1]
+			if len(line) > 0 {
+				c = line[len(line) - 1]
+			}
+		}
+		editorAppendRow(line)
+	}
+
+	if err != nil && err != io.EOF {
 		die(err)
 	}
-
-	for c := line[len(line) - 1]; len(line) > 0 && (c == '\n' || c == '\r'); line = line[:len(line)-1] {
-	}
-
-	editorAppendRow(line)
 }
 
 /*** input ***/
@@ -325,11 +330,14 @@ func editorRefreshScreen() {
 	editorDrawRows(&ab)
 	ab.abAppend(fmt.Sprintf("\x1b[%d;%dH", E.cy+1, E.cx+1))
 	ab.abAppend("\x1b[25h")
-	io.WriteString(os.Stdout, ab.String())
+	_, e := io.WriteString(os.Stdout, ab.String())
+	if e != nil {
+		log.Fatal(e)
+	}
 }
 
 func editorDrawRows(ab *abuf) {
-	for y := 0; y < E.screenRows-1; y++ {
+	for y := 0; y < E.screenRows; y++ {
 		if y >= E.numRows {
 			if E.numRows == 0 && y == E.screenRows/3 {
 				w := fmt.Sprintf("Kilo editor -- version %s", KILO_VERSION)
@@ -346,9 +354,9 @@ func editorDrawRows(ab *abuf) {
 				ab.abAppend("~")
 			}
 		} else {
-			length := len(E.rows.chars)
+			length := len(E.rows[y].chars)
 			if length > E.screenCols { length = E.screenCols }
-			ab.abAppendBytes(E.rows.chars[:length])
+			ab.abAppendBytes(E.rows[y].chars[:length])
 		}
 		ab.abAppend("\x1b[K")
 		if y < E.screenRows-1 {
